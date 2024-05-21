3898c7af02d467418d3ab8a17009e4fc5828801f
hugostajer <hugostajer@gmail.com>
2024-05-21 13:15:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ function zapisAktualniDatum() {
   var den = d.getDate();
   var mesic = d.getMonth() + 1; // Měsíce jsou indexovány od 0, takže přidáváme 1
   var rok = d.getFullYear();
+  var formatovaneDatum = den + '.' + mesic + '.' + rok;
+  document.getElementById("aktualni-datum").innerHTML = formatovaneDatum;
+}
+window.onload = function() {
+  zapisAktualniDatum();
+};
